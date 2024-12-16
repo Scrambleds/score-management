e2f53e7cd152b252b7ac92ba2f2650051570b988
firstfear1412 <78691900+firstfear1412@users.noreply.github.com>
2024-12-17 03:17:39 +0700

    update config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://api.production.com', // URL สำหรับ Production
+  apiUrl: 'https://app2.sci.src.ku.ac.th/scoreAPI', // URL สำหรับ Production
 };
